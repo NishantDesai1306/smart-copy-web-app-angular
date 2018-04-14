71b56d711ca5159de2ca42ea8571ead0774b9d77
Nishant Desai <nishantdesai1306@gmail.com>
2018-04-14 06:13:52 +0530

    removed logs

--- server/auth/google.verify.js
+++ server/auth/google.verify.js
@@ -6,21 +6,19 @@ var config = require('../config');
 exports.verify = function (token, email) {
     var defer = Q.defer();
 
-    console.log('making request');
     request('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token='+token, function(err, res, body) {
         
         if (err) {
-            console.log('geot error', err);
             return defer.reject(err);
         }
 
         body = JSON.parse(body);
-        console.log(JSON.stringify(body.audience));
-        console.log(JSON.stringify(config.googleAppId));
-        console.log(body.audience === config.googleAppId);
-        console.log(JSON.stringify(body.email));
-        console.log(JSON.stringify(email));
-        console.log(body.email === email);
+        // console.log(JSON.stringify(body.audience));
+        // console.log(JSON.stringify(config.googleAppId));
+        // console.log(body.audience === config.googleAppId);
+        // console.log(JSON.stringify(body.email));
+        // console.log(JSON.stringify(email));
+        // console.log(body.email === email);
         if (body.audience === config.googleAppId && body.email === email) {
             return defer.resolve();
         }
