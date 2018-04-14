a6d00918a329b135fc925f8e9a8d3407a6744712
Nishant Desai <nishantdesai1306@gmail.com>
2018-04-14 05:32:58 +0530

    updated google api key

--- client/app/shared/constants.service.ts
+++ client/app/shared/constants.service.ts
@@ -1,4 +1,4 @@
 export class Constants {
     public static FACEBOOK_APP_ID = '1160752057371490';
-    public static GOOGLE_CLIENT_ID = '657668041083-3c3km4753dm7tfavhdmru00p51on64i7.apps.googleusercontent.com';
+    public static GOOGLE_CLIENT_ID = '657668041083-7mhmu5aurqche7a1911ed5qsmg5o2ubt.apps.googleusercontent.com';
 }
